e66690bf21e8c75833ae4eb2e3f9a5bedfe131ee
andela-fokosun <okosunuzflorence@gmail.com>
2018-01-21 18:05:07 +0100

    view to display al files across app and download link

--- resources/views/projects/showAllFiles.blade.php
+++ resources/views/projects/showAllFiles.blade.php
@@ -5,5 +5,38 @@
 @stop
 
 @section('content')
-
+<div class="box box-primary">
+    <div class="box-body no-padding">
+        @if( count($files) === 0 )
+            There are no files.
+        @else
+        <table class="table table-bordered table-responsive">
+            <thead>
+            <tr>
+                <th>File Name</th>
+                <th>Date uploaded</th>
+                <th>Project</th>
+                <th>
+                    --
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($files as $file)
+                <tr>
+                    <td>{{ $file->original_name }}</td>
+                    <td>{{ $file->created_at }}</td>
+                    <td>{{ $file->project->name }}</td>
+                    <td>
+                        <a href="{{ URL::to( '/download/' . $file->fileName)  }}" target="_blank">
+                            Download
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+            @endif
+        </table>
+    </div>
+</div>
 @stop
