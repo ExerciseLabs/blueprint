839cb29a5f46dc566e8067e61d544ce9337d8202
andela-fokosun <okosunuzflorence@gmail.com>
2018-01-21 17:25:32 +0100

    add FileUpload Interface

--- app/Interfaces/FileEloquentInterface.php
+++ app/Interfaces/FileEloquentInterface.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * FileEloquentInterface
- *
- * @author Florence Okosun <florence.okosun@getitllc.com>
- * @copyright Copyright(c) 2018 Get It, LLC(http://www.getit.me)
- */
 
 namespace App\Interfaces;
 
-
-class FileEloquentInterface
+interface FileEloquentInterface extends BaseEloquentInterface
 {
 
 }
